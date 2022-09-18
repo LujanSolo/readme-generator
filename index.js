cd8329e93e7b7e47ec3a7cb9ac2de793a676e7b4
LujanSolo <lujansolo@proton.me>
2022-09-17 17:44:13 -0700

    added requirements and began questions array

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 // TODO: Include packages needed for this application
-const generateMarkdown = 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs"); //*read and write files to dist (fs.writeFileSync  <-- most common)
 
+const path = require("path")
+const inquirer = require("inquirer") //* user-interface, effectively
+
+
+//*making 2 types of inquirer prompts
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What type of License would you like to use?',
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD3", "none"],
+  }
+];
+//todo must be a dynamic reaction to "none" with license, as you won't have the badge, no section on license, and no table of contents link 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
