b262cbf35340241740430ff5310f7091ac26517f
LujanSolo <lujansolo@proton.me>
2022-09-18 19:44:02 -0700

    added readME text to generateMarkdown as static text. Built init function

--- index.js
+++ index.js
@@ -1,95 +1,92 @@
 // TODO: Include packages needed for this application
 const generateMarkdown = require("./utils/generateMarkdown");
-const fs = require("fs"); //*read and write files to dist (fs.writeFileSync  <-- most common)
-
+const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
-
 
 //*making 2 types of inquirer prompts
 // TODO: Create an array of questions for user input
 const questions = [
   {
-    type: 'input',
-    name: 'title',
+    type: "input",
+    name: "title",
     message: "What is your project's name?",
   },
   {
-    type: 'input',
-    name: 'description',
-    message: 'Provide a short description explaining the what, why, and how of your project.',
+    type: "input",
+    name: "description",
+    message:
+      "Provide a short description explaining the what, why, and how of your project.",
   },
   {
-    type: 'input',
-    name: 'installation',
-    message: 'What are the steps required to install your project?'
+    type: "input",
+    name: "installation",
+    message: "What are the steps required to install your project?",
   },
   {
-    type: 'input',
-    name: 'usage',
-    message: 'Provide instructions and examples for use.',
+    type: "input",
+    name: "usage",
+    message: "Provide instructions and examples for use.",
   },
   {
-    type: 'input',
-    name: 'contribution',
-    message: 'How can others contribute to this project?',
+    type: "input",
+    name: "contribution",
+    message: "How can others contribute to this project?",
   },
   {
-    type: 'input',
-    name: 'testing',
-    message: 'If you have written tests for your app, provide examples on how to run them:',
+    type: "input",
+    name: "testing",
+    message:
+      "If you have written tests for your app, provide examples on how to run them:",
   },
   {
-    type: 'list',
-    name: 'license',
-    message: 'What type of License would you like to use?',
+    type: "list",
+    name: "license",
+    message: "What type of License would you like to use?",
     choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD3", "none"], //*make this a selectable list
   },
   {
-  type: 'input',
-  name: 'gitUsername',
-  message: 'What is your GitHub username?',
+    type: "input",
+    name: "gitUsername",
+    message: "What is your GitHub username?",
   },
   {
-    type: 'input',
-    name: 'email',
-    message: 'What is your email address?', //*these last 2 added to QUESTIONS section of readme
+    type: "input",
+    name: "email",
+    message: "What is your email address?", //*these last 2 added to QUESTIONS section of readme
   },
 ];
 //* must be a dynamic reaction to "none" with license, as there won't be a badge, no section on license, and no table of contents link (bonus?)
 
-
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(), "/dist", fileName), data, (err) =>
-  err ? console.error(err) : console.log('File successfully created.'))
-};
+  return fs.writeFileSync(
+    path.join(process.cwd(), "/dist", fileName),
+    data,
+    (err) =>
+      err ? console.error(err) : console.log("File successfully created.")
+  );
+}
 
-//*absolute path = path.resolve
-//*relative path = path.join
 
-// TODO: Create a function to initialize app
-//*this is where I do my inquire prompt, pass in my questions
-//* follow with .then and take back the requirer (res)ponses, as inquirer will return the res object with all of the answers inside of it
-//* access each one via the name (.then response.email, etc)
-//* then call writeToFile ; pass it generateMarkdown for the data
-//* pass generateMarkdown(inquirerResponses)
+
+//* FUNCTION to initialize app
 function init() {
-
-inquirer
-.prompt(questions)
-.then((answers) => {
-  console.log(answers);
-})
-.catch((error) => {
-  if (error.isTtyError) {
-    // Prompt couldn't be rendered in the current environment
-    console.log('An error occurred.');
-  } else {
-    // Something else went wrong
-    console.log('An error occurred.');
-  }
-});
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      const markdownOutput = generateMarkdown(answers);
+      writeToFile('README.md', markdownOutput)
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        console.log('An error occurred.');
+      } else {
+        // Something else went wrong
+        console.log('An error occurred.');
+      }
+    });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -29,6 +29,42 @@ function generateMarkdown(data) {
   return `
   # ${data.title}
 
+  ## Description $(data.)
+
+  ## Table of Contents (Optional)
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+
+  ## Usage
+
+  To add a screenshot, create an `assets / images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+  ![alt text](assets / images / screenshot.png)
+    ```
+
+  ## Credits
+
+  List your collaborators, if any, with links to their GitHub profiles.
+
+  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+  If you followed tutorials, include links to those here as well.
+
+  ## License
+
+
+  ## How to Contribute
+
+  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+  ## Tests
+
   ${renderLicenseSection(data)}
 
   ${renderLicenseBadge(data)}
@@ -36,6 +72,8 @@ function generateMarkdown(data) {
   # How to Contribute
 
   If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+  ## Questions
 
 `;
 }
@@ -45,40 +83,6 @@ module.exports = generateMarkdown;
 
 
 
-// ## Description
 
-// ## Table of Contents (Optional)
-
-// - [Installation](#installation)
-// - [Usage](#usage)
-// - [Credits](#credits)
-// - [License](#license)
-
-// ## Installation
-
-// ## Usage
-
-// To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-//     ```md
-//     ![alt text](assets/images/screenshot.png)
-//     ```
-
-// ## Credits
-
-// List your collaborators, if any, with links to their GitHub profiles.
-
-// If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-// If you followed tutorials, include links to those here as well.
-
-// ## License
-
-
-// ## How to Contribute
-
-// If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
-// ## Tests
 
 
